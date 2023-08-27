748bec9a82133122e51294066811fba1edb48ff9
Willie Alcaraz <63901327+ProjectZuki@users.noreply.github.com>
2023-08-26 18:09:47 -0700

    26 August Update
    Animated the titles successfully, cleaned up code for animations
    
    NOTE: Still working on original title animation, for some reason modifying "Las Vegas, Nevada" will completely crash it. Will sell as a feature...

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const content = document.querySelector(".content");
   const aboutSection = document.querySelector(".about-text");
 
+  // ------------------------ cursor text animations ------------------------ //
+
+  // --- title typing animation ---
+
   // Text for typing animation
   const foregroundText_line1 = "Las Vegas, Nevada";
   const foregroundText_line2 = "Website & Software Development/Engineering | UNLV Computer Science";
@@ -14,32 +18,139 @@ document.addEventListener("DOMContentLoaded", () => {
   const foreground_line2Span = document.getElementById("typing-text-line2");
   const cursorElement = document.getElementById("cursor-animation");
 
-  // Set delay for start of typing animation
-setTimeout(() => {
-  typeText(foregroundText_line1, foreground_line1Span, cursorElement, 0, 75, () => {
-    setTimeout(() => {
-      typeText(foregroundText_line2, foreground_line2Span, cursorElement, 0, 50, () => {
-        cursorElement.style.opacity = "1"; // Show cursor with blinking animation
-        setInterval(() => {
-          cursorElement.style.opacity = cursorElement.style.opacity === "0" ? "1" : "0";
-        }, 500);
-      });
-    }, 500); // Delay before starting line2 typing
-  });
-}, 1750);
+  // Show the text "on line 1" without typing animation
+  foreground_line1Span.textContent = foregroundText_line1;
 
-// Function to create typing animation
-function typeText(text, spanElement, cursorElement, index, typeSpeed, callback) {
-  if (index < text.length) {
-    spanElement.textContent += text[index];
-    index++;
-    setTimeout(() => {
-      typeText(text, spanElement, cursorElement, index, typeSpeed, callback);
-    }, typeSpeed);
-  } else {
-    callback(); // Call the callback function once typing is done
+  // Set delay for start of typing animation for line2
+  setTimeout(() => {
+    typeText(foregroundText_line2, foreground_line2Span, cursorElement, 0, 50, () => {
+      cursorElement.style.opacity = "1"; // Show cursor with blinking animation
+      setInterval(() => {
+        cursorElement.style.opacity = cursorElement.style.opacity === "0" ? "1" : "0";
+      }, 500);
+    });
+  }, 1750); // Delay before starting line2 typing
+
+  // Function to create typing animation
+  function typeText(text, spanElement, cursorElement, index, typeSpeed, callback) {
+    if (index < text.length) {
+      spanElement.textContent += text[index];
+      index++;
+      setTimeout(() => {
+        typeText(text, spanElement, cursorElement, index, typeSpeed, callback);
+      }, typeSpeed);
+    } else {
+      callback(); // Call the callback function once typing is done
+    }
   }
-}
+
+  // --- "About" typing animation ---
+
+  // required variables for elements
+  const aboutText = "About";  // text to be animated
+  const aboutTitleSpan = document.getElementById("about-text-line");  // id of title to be animated
+  const aboutCursorElement = document.getElementById("about-cursor-animation"); // id of cursor element for animation
+
+  /**
+   * Function to create typing animation for specifed title text
+   * @param {*} text the string to be animated
+   * @param {*} spanElement the span element of the text
+   * @param {*} cursorElement cursor element of the text
+   * @param {*} index current index of character
+   * @param {*} typeSpeed speed of which animation should appear to "type"
+   * @param {*} callback callback to execute when the string is finished processing
+   */
+  function animnateTitleText(text, spanElement, cursorElement, index, typeSpeed, callback) {
+    if (index < text.length) {
+      spanElement.textContent += text[index];
+      index++;
+      setTimeout(() => {
+        animnateTitleText(text, spanElement, cursorElement, index, typeSpeed, callback);
+      }, typeSpeed);
+    } else {
+      callback(); // Call the callback function once typing is done
+    }
+  }
+
+  // Intersection observer for the "About" title section
+  const aboutTitleObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // set delay for when we scroll, to the beginning of the animation
+        setTimeout(() => {
+          animnateTitleText(aboutText, aboutTitleSpan, aboutCursorElement, 0, 75, () => {
+            aboutCursorElement.style.opacity = "1"; // Show cursor with blinking animation
+            setInterval(() => {
+              aboutCursorElement.style.opacity = aboutCursorElement.style.opacity === "0" ? "1" : "0";
+            }, 500);
+          });
+        }, 1000); // modify delay for typing animation to begin once initiated
+
+        // Unobserve the section to prevent unnecessary animations
+        aboutTitleObserver.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.5 });
+
+  // Observe the "About" title section
+  aboutTitleObserver.observe(document.querySelector(".about-text-title"));
+
+  // --- "Gallery" title animation ---
+
+  // Text for "Gallery" title animation
+  const galleryText = "Gallery";
+  const galleryTitleSpan = document.getElementById("gallery-text-line");
+  const galleryCursorElement = document.getElementById("gallery-cursor-animation");
+
+  // Intersection observer for the "Gallery" title section
+  const galleryTitleObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          animnateTitleText(galleryText, galleryTitleSpan, galleryCursorElement, 0, 50, () => {
+            galleryCursorElement.style.opacity = "1"; // Show cursor with blinking animation
+            setInterval(() => {
+              galleryCursorElement.style.opacity = galleryCursorElement.style.opacity === "0" ? "1" : "0";
+            }, 500);
+          });
+        }, 1000); // modify delay
+
+        // Unobserve the section to prevent unnecessary animations
+        galleryTitleObserver.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 1.0 }); // threshold when to start animation
+
+  // Observe the "Gallery" title section once gallery-text-title has been scrolled to
+  galleryTitleObserver.observe(document.querySelector(".gallery-text-title"));
+
+  // --- Projects title animation ---
+
+  // repeat above processes
+  const projectsText = "Projects";
+  const projectsTitleSpan = document.getElementById("projects-text-line");
+  const projectsCursorElement = document.getElementById("projects-cursor-animation");
+
+  const projectsTitleObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          animnateTitleText(projectsText, projectsTitleSpan, projectsCursorElement, 0, 50, () => {
+            setInterval(() => {
+              projectsCursorElement.style.opacity = projectsCursorElement.style.opacity == "0"? "1" : "0";
+            }, 500);
+          });
+        }, 1000);  //modify delay
+
+        // unobserve section
+        projectsTitleObserver.unobserve(entry.target);
+      }
+    });
+  }, {threshold: 1.0});
+
+  projectsTitleObserver.observe(document.querySelector(".projects-text-title"));
+
+  // ---------------------- end cursor text animations ---------------------- //
 
   // artificial oading screen
 
@@ -56,7 +167,7 @@ function typeText(text, spanElement, cursorElement, index, typeSpeed, callback) 
   }, 500) // delay time for loading animation
 
   const imageSections = document.querySelectorAll(".image-section");
-  const firstImageSection = document.querySelector(".image-section");
+  const firstImageSection = document.querySelector(".gallery-view");
 
   imageSections.forEach(imageSection => {
     const animatedImages = imageSection.querySelectorAll('.animated-image');
